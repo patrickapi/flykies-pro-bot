2b6684136a7abe6fba7b0d8af96ff5ae456c375c
patrickapi <74774839+patrickapi@users.noreply.github.com>
2023-08-18 19:09:57 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  "token":  "NjMxODg5MjQwOTc1ODY3OTI1.XZ9atg.r1Nj2WZs7w8UCL3CimcwEdFAggU",
+  "token":  Kendi Tokeninizi Buraya Yazın",
   "geliştiriciler": ["Kendi Discord ID'nizi buraya yazın."], 
-  "prefix": "f!",
-  "logo": "https://cdn.glitch.com/28770909-8acc-4da4-9b5e-2397802348b7%2Ff80a01cb-b136-4839-bdcc-b269e9c234d2.image.png?1619034088593",
-  "link": "https://discord.com/oauth2/authorize?client_id=631889240975867925&scope=bot&permissions=8"
+  "prefix": "Prefixinizi buraya yazınız",
+  "logo": Botunuzun logo linkini koyunuz",
+  "link": Botunuzun Linkini koyunuz"
 };
